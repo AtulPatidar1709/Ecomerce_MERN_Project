1581213683742e0942dbb474576f5ecc641cee73
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2024-08-25 18:59:04 +0530

    Remove dist files from tracking

--- ecommerce-backend/dist/utils/utility-class.js
+++
@@ -1,9 +0,0 @@
-class errorHandler extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.message = message;
-        this.statusCode = statusCode;
-        this.statusCode = statusCode;
-    }
-}
-export default errorHandler;
